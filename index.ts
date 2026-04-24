5457019b37d2fabca8673895fc56bd85219703cb
jspmern <jspmern@gmail.com>
2026-04-24 14:20:13 +0530

    added llm and state and package

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 
+import { ChatOpenAI } from "@langchain/openai"
+
+const llm = new ChatOpenAI({
+  model: "gpt-5.4-nano-2026-03-17",
+})
 async function initializeModal()
 {
      //modal call will come  
