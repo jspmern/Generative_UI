5797e3679be197a7c263490acc67f09c3148fe6e
jspmern <jspmern@gmail.com>
2026-05-07 20:16:13 +0530

    add sse in client and backend side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ChatContainer } from './component/ChatContainer';
 
 function App() {
+  useEffect(()=>{
+     const eventSource = new EventSource("http://localhost:3000/chat");
+     eventSource.addEventListener("open",()=>{
+      console.log("Connection opened");
+     })
+      eventSource.addEventListener("message",(data)=>{
+        console.log("Received data",data)
+      })
+       eventSource.addEventListener("error",(err)=>{
+           console.error("Error",err)
+       })
+  },[])
    return <ChatContainer />;
 }
 
--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import { dbConnection } from "./config/db"
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
 import { app } from "./graph/graph"
 /**maintain session */
 /**update stream */
@@ -15,7 +18,7 @@ import { app } from "./graph/graph"
 const config=  { configurable: { thread_id: "1" }, streamMode: ["updates","custom"] }
 
 
-
+dotenv.config()
 dbConnection()
 
 async function main()
@@ -29,5 +32,43 @@ async function main()
     //console.log('result',JSON.stringify(result,null,2))
     //console.log(result.messages[result.messages.length-1]?.content)
 }
-main()
+
+const server=express()
+server.use(express.json())
+server.use(cors())
+server.get("/chat",(req,res)=>{
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+    res.flushHeaders();
+      const sendData = () => {
+
+    const data = {
+      time: new Date(),
+      random: Math.random(),
+    };
+
+    res.write(`data: ${JSON.stringify(data)}\n\n`);
+  };
+
+  const interval = setInterval(sendData, 1000);
+
+  // heartbeat
+  const heartbeat = setInterval(() => {
+    res.write(": ping\n\n");
+  }, 2000);
+
+  req.on("close", () => {
+
+    clearInterval(interval);
+    clearInterval(heartbeat);
+
+    res.end();
+  });
+})
+server.listen(3000,()=>{
+    console.log("server is running on port 3000")
+    // main()
+})
+
 
