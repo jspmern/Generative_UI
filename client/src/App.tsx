9cac1508c76a07976fcabbadffe3ed09f9272525
jspmern <jspmern@gmail.com>
2026-05-08 15:21:27 +0530

    fe: send payload to backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,46 @@
-import React, { useEffect } from 'react'
-import { ChatContainer } from './component/ChatContainer';
+import React, { useEffect } from "react";
+import { ChatContainer } from "./component/ChatContainer";
+import { fetchEventSource } from "@microsoft/fetch-event-source";
 
 function App() {
-  useEffect(()=>{
-     const eventSource = new EventSource("http://localhost:3000/chat");
-     eventSource.addEventListener("open",()=>{
-      console.log("Connection opened");
-     })
-      eventSource.addEventListener("message",(data)=>{
-        console.log("Received data",data)
-      })
-       eventSource.addEventListener("error",(err)=>{
-           console.error("Error",err)
-       })
-  },[])
-   return <ChatContainer />;
+    useEffect(() => {
+
+    async function fetchData() {
+
+      await fetchEventSource("http://localhost:3000/chat", {
+
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({
+          message: "hello world",
+        }),
+
+        async onopen() {
+          console.log("Connection opened");
+        },
+
+        onmessage(ev) {
+          console.log("Received:", ev.data);
+        },
+
+        onerror(err) {
+          console.log("Error:", err);
+        },
+
+        onclose() {
+          console.log("Connection closed");
+        },
+      });
+    }
+
+    fetchData();
+
+  }, []);
+  return <ChatContainer />;
 }
 
-export default App
+export default App;
